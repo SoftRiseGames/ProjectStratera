0ba0b592d53aa2755943faae9ccf955f34de6caa
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-07-08 02:48:44 +0300

    UpdatedStrateraProject

--- Assets/Scripts/Level4Bosses.cs
+++ Assets/Scripts/Level4Bosses.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rightleft : MonoBehaviour
+public class Level4Bosses : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rb;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
