f8abd8c32f29085596f39c6afaada7ea1a19129d
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-07-08 01:44:44 +0300

    UpdatedStrateraProject

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,17 +22,23 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            DeathControl = DeathControl - 1;
-        }
+        
         if (Input.GetMouseButtonDown(0) && arraycontrol > 0)
         {
             health[arraycontrol-1].gameObject.SetActive(false);
             arraycontrol = arraycontrol - 1;
         }
+        else if(Input.GetMouseButtonDown(0) && arraycontrol == 0)
+        {
+            health[0].gameObject.SetActive(false);
+            
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            DeathControl = DeathControl - 1;
+        }
         StartCoroutine(timer());
-        Debug.Log(DeathControl);
+        
     }
     public IEnumerator timer()
     {
