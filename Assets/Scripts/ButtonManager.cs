60ebdc0a808c1deb213f35d3dda2245fa1760546
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-07-18 15:21:48 +0300

    levelsbuttons

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,6 +7,7 @@ public class ButtonManager : MonoBehaviour
 {
     public MainMenuScript mainmenumanager;
     public SaveGame saveManager;
+    public GameObject levelsAll;
    public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,5 +19,13 @@ public class ButtonManager : MonoBehaviour
     public void welcome()
     {
         StartCoroutine(mainmenumanager.welcome());
+    }
+    public void levels()
+    {
+        levelsAll.gameObject.SetActive(true);
+    }
+    public void levelclose()
+    {
+        levelsAll.gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelManager : MonoBehaviour
+{
+    public Scene[] scenes;
+    public Button[] buttons;
+    void Start()
+    {
+        PlayerPrefs.GetInt("levelbuttons");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i =0; i < buttons.Length && i<PlayerPrefs.GetInt("go");)
+        {
+            buttons[i].gameObject.SetActive(true);
+            i++;
+            PlayerPrefs.SetInt("levelbuttons", i);
+        }
+    }
+}
