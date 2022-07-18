8698beae7798836dc3bae201be65afbcb633bde8
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-07-18 13:51:48 +0300

    Revert "Revert "Revert "buttonPos"""
    This reverts commit b07e1b2868338532f3c8ba69775d11228a881327.

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,9 +7,6 @@ public class ButtonManager : MonoBehaviour
 {
     public MainMenuScript mainmenumanager;
     public SaveGame saveManager;
-    public GameObject TimeLineLevels;
-    public GameObject Levels;
-    public GameObject levelbuttons;
    public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -21,11 +18,5 @@ public class ButtonManager : MonoBehaviour
     public void welcome()
     {
         StartCoroutine(mainmenumanager.welcome());
-    }
-    public void levels()
-    {
-
-        Levels.gameObject.SetActive(true);
-        levelbuttons.gameObject.SetActive(true);
     }
 }
