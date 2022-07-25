0ad03769cc2c18b1a4b1e53e31e9b5256a79a6ed
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-07-25 14:25:06 +0300

    Update MainMenuScript.cs

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -12,74 +12,66 @@ public class MainMenuScript : MonoBehaviour
 
     private void Start()
     {
-        TimeLineWelcome.gameObject.SetActive(false);
-
-        private void Start()
+      TimeLineWelcome.gameObject.SetActive(false);
+      Debug.Log(PlayerPrefs.GetInt("levelvolume"));
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            TimeLineWelcome.gameObject.SetActive(false);
-            Debug.Log(PlayerPrefs.GetInt("levelvolume"));
-
-
+            savebool.value = 0;
+            PlayerPrefs.SetInt("isstartgame", savebool.value);
+            Debug.Log(PlayerPrefs.GetInt("isstartgame"));
         }
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                savebool.value = 0;
-                PlayerPrefs.SetInt("isstartgame", savebool.value);
-                Debug.Log(PlayerPrefs.GetInt("isstartgame"));
-            }
 
+        if (PlayerPrefs.GetInt("levelvolume") == 1)
+        {
+            music.gameObject.SetActive(true);
+        }
+        else if (PlayerPrefs.GetInt("levelvolume") == 0)
+        {
+            music.gameObject.SetActive(false);
+        }
+    }
+    public IEnumerator welcome()
+    {
+        TimeLineWelcome.gameObject.SetActive(true);
+
+        if (PlayerPrefs.GetInt("isstartgame") == 0)
+        {
+            yield return new WaitForSeconds(1.5f);
+            SceneManager.LoadScene(1);
+            savebool.value = 1;
+            PlayerPrefs.SetInt("isstartgame", savebool.value);
+            Debug.Log(PlayerPrefs.GetInt("isstartgame"));
             if (PlayerPrefs.GetInt("levelvolume") == 1)
             {
-                music.gameObject.SetActive(true);
+                gameplayMusic.gameObject.SetActive(true);
+                DontDestroyOnLoad(gameplayMusic);
             }
             else if (PlayerPrefs.GetInt("levelvolume") == 0)
             {
-                music.gameObject.SetActive(false);
+                gameplayMusic.gameObject.SetActive(false);
             }
+
         }
-        public IEnumerator welcome()
+
+        if (PlayerPrefs.GetInt("isstartgame") == 1)
         {
-            TimeLineWelcome.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1.5f);
+            SceneManager.LoadScene(PlayerPrefs.GetInt("go"));
 
-            if (PlayerPrefs.GetInt("isstartgame") == 0)
+            if (PlayerPrefs.GetInt("levelvolume") == 1)
             {
-                yield return new WaitForSeconds(1.5f);
-                SceneManager.LoadScene(1);
-                savebool.value = 1;
-                PlayerPrefs.SetInt("isstartgame", savebool.value);
-                Debug.Log(PlayerPrefs.GetInt("isstartgame"));
-                if (PlayerPrefs.GetInt("levelvolume") == 1)
-                {
-                    gameplayMusic.gameObject.SetActive(true);
-                    DontDestroyOnLoad(gameplayMusic);
-                }
-                else if (PlayerPrefs.GetInt("levelvolume") == 0)
-                {
-                    gameplayMusic.gameObject.SetActive(false);
-                }
-
+                gameplayMusic.gameObject.SetActive(true);
+                DontDestroyOnLoad(gameplayMusic);
             }
-
-            if (PlayerPrefs.GetInt("isstartgame") == 1)
+            else if (PlayerPrefs.GetInt("levelvolume") == 0)
             {
-                yield return new WaitForSeconds(1.5f);
-                SceneManager.LoadScene(PlayerPrefs.GetInt("go"));
-
-                if (PlayerPrefs.GetInt("levelvolume") == 1)
-                {
-                    gameplayMusic.gameObject.SetActive(true);
-                    DontDestroyOnLoad(gameplayMusic);
-                }
-                else if (PlayerPrefs.GetInt("levelvolume") == 0)
-                {
-                    gameplayMusic.gameObject.SetActive(false);
-                }
+                gameplayMusic.gameObject.SetActive(false);
             }
-
-
         }
+
 
     }
 }
