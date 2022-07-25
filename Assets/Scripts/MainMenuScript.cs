c4b37f38df59acb3cdabdc9f526312becce4f283
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-07-25 14:17:58 +0300

    Update MainMenuScript.cs

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -9,76 +9,77 @@ public class MainMenuScript : MonoBehaviour
     public GameObject music;
     public GameObject gameplayMusic;
 
-    
-    private void Start()
-    {
-        TimeLineWelcome.gameObject.SetActive(false);
 
     private void Start()
     {
         TimeLineWelcome.gameObject.SetActive(false);
-        Debug.Log(PlayerPrefs.GetInt("levelvolume"));
 
->>>>>>> parent of 50d424e (Particles)
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
+        private void Start()
         {
-            savebool.value = 0;
-            PlayerPrefs.SetInt("isstartgame", savebool.value);
-            Debug.Log(PlayerPrefs.GetInt("isstartgame"));
-        }
+            TimeLineWelcome.gameObject.SetActive(false);
+            Debug.Log(PlayerPrefs.GetInt("levelvolume"));
 
-        if(PlayerPrefs.GetInt("levelvolume") == 1)
-        {
-            music.gameObject.SetActive(true);
+
         }
-        else if (PlayerPrefs.GetInt("levelvolume") == 0)
+        private void Update()
         {
-            music.gameObject.SetActive(false);
-        }
-    }
-    public IEnumerator welcome()
-    {
-        TimeLineWelcome.gameObject.SetActive(true);
-       
-        if(PlayerPrefs.GetInt("isstartgame") == 0)
-        {
-            yield return new WaitForSeconds(1.5f);
-            SceneManager.LoadScene(1);
-            savebool.value = 1;
-            PlayerPrefs.SetInt("isstartgame", savebool.value);
-            Debug.Log(PlayerPrefs.GetInt("isstartgame"));
-            if (PlayerPrefs.GetInt("levelvolume") == 1)
+            if (Input.GetKeyDown(KeyCode.R))
             {
-                gameplayMusic.gameObject.SetActive(true);
-                DontDestroyOnLoad(gameplayMusic);
+                savebool.value = 0;
+                PlayerPrefs.SetInt("isstartgame", savebool.value);
+                Debug.Log(PlayerPrefs.GetInt("isstartgame"));
             }
-            else if (PlayerPrefs.GetInt("levelvolume") == 0)
-            {
-                gameplayMusic.gameObject.SetActive(false);
-            }
-
-        }
-      
-        if(PlayerPrefs.GetInt("isstartgame") == 1)
-        {
-            yield return new WaitForSeconds(1.5f);
-            SceneManager.LoadScene(PlayerPrefs.GetInt("go"));
 
             if (PlayerPrefs.GetInt("levelvolume") == 1)
             {
-                gameplayMusic.gameObject.SetActive(true);
-                DontDestroyOnLoad(gameplayMusic);
+                music.gameObject.SetActive(true);
             }
             else if (PlayerPrefs.GetInt("levelvolume") == 0)
             {
-                gameplayMusic.gameObject.SetActive(false);
+                music.gameObject.SetActive(false);
             }
         }
-       
+        public IEnumerator welcome()
+        {
+            TimeLineWelcome.gameObject.SetActive(true);
+
+            if (PlayerPrefs.GetInt("isstartgame") == 0)
+            {
+                yield return new WaitForSeconds(1.5f);
+                SceneManager.LoadScene(1);
+                savebool.value = 1;
+                PlayerPrefs.SetInt("isstartgame", savebool.value);
+                Debug.Log(PlayerPrefs.GetInt("isstartgame"));
+                if (PlayerPrefs.GetInt("levelvolume") == 1)
+                {
+                    gameplayMusic.gameObject.SetActive(true);
+                    DontDestroyOnLoad(gameplayMusic);
+                }
+                else if (PlayerPrefs.GetInt("levelvolume") == 0)
+                {
+                    gameplayMusic.gameObject.SetActive(false);
+                }
+
+            }
+
+            if (PlayerPrefs.GetInt("isstartgame") == 1)
+            {
+                yield return new WaitForSeconds(1.5f);
+                SceneManager.LoadScene(PlayerPrefs.GetInt("go"));
+
+                if (PlayerPrefs.GetInt("levelvolume") == 1)
+                {
+                    gameplayMusic.gameObject.SetActive(true);
+                    DontDestroyOnLoad(gameplayMusic);
+                }
+                else if (PlayerPrefs.GetInt("levelvolume") == 0)
+                {
+                    gameplayMusic.gameObject.SetActive(false);
+                }
+            }
+
+
+        }
 
     }
-   
 }
